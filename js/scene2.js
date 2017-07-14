b2e4a9d92e49a5048c22412e20dafec590a03f29
mikesmall <michael.l.a.small@gmail.com>
2017-07-13 22:49:40 -0400

    Ned Stark, standing on some rocks

--- js/scene2.js
+++ js/scene2.js
@@ -11,10 +11,30 @@ function scene2() {
   stageImg.setAttribute('src', castleImgSrc);
 
   // Setup Cast
-
   // Character 1
+  var nedContainer  = document.createElement('div');
+  var ned           = document.querySelector('#ned');
+  var leather       = document.querySelector('#leather-armour');
+
+  nedContainer.style.position = 'absolute';
+  nedContainer.style.width = '10%';
+  nedContainer.style.height = '20%';
+  nedContainer.style.top = '50%';
+  nedContainer.style.left = '50%';
+  nedContainer.style.height = '37%';
+  nedContainer.style.top = '55%';
+  nedContainer.style.left = '25%';
+  ned.style.position = 'absolute';
+  ned.style.top = 0;
+  ned.style.width = '70%';
+  leather.style.position = 'absolute';
+  leather.style.bottom = 0;
+  stage.append(nedContainer);
+  nedContainer.append(ned);
+  nedContainer.append(leather);
 
   // Character 2
+  
 
   // Setup Props
 
