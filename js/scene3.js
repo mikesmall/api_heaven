ff823bd31ecebedbb6482e4448d25b6d8832345b
mikesmall <michael.l.a.small@gmail.com>
2017-07-13 23:41:33 -0400

    Danerys Stormborn onscreen now

--- js/scene3.js
+++ js/scene3.js
@@ -5,12 +5,38 @@ function scene3() {
   stageImg = document.querySelector('#stage-img')
   stageImg.setAttribute('src', mySrc)
 
+  // Using the same steps as you used to construct Scene 2, create a scene with Danerys and Drogo playing catch with the dragon's egg.
 
   // Setup Cast
+  // Character 1 - Danerys Stormborn
+  var danerysContainer  = document.createElement( 'div' );
+  var danerys           = document.querySelector( '#danerys' );
+  var blueDress         = document.querySelector( '#blue-dress' );
 
-  // Character 1
+  danerysContainer.style.position = 'absolute';
+  danerysContainer.style.width    = '10%';
+  danerysContainer.style.height   = '20%';
+  danerysContainer.style.top      = '50%';
+  danerysContainer.style.left     = '50%';
+  danerysContainer.style.height   = '37%';
+  danerysContainer.style.top      = '55%';
+  danerysContainer.style.left     = '25%';
 
-  // Character 2
+  danerys.style.position     = 'absolute';
+  danerys.style.top          = 0;
+  danerys.style.width        = '60%';
+
+  blueDress.style.position = 'absolute';
+  blueDress.style.bottom   = 0;
+
+  stage.append(danerysContainer);
+  danerysContainer.append(danerys);
+  danerysContainer.append(blueDress);
+
+  // Character 2 - Drogo the Weird Jerk
+  var drogoContainer    = document.createElement( 'div' );
+  var drogo             = document.querySelector( '#drogo' );
+  var bulkyMan          = document.querySelector( '#bulky-man' );
 
   // Setup Props
 
